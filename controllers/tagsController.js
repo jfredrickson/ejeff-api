c7d62f0f3df43ee6b077772730d74c9c0d02f3a8
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2018-10-19 13:49:54 -0400

    add tags apidocs

--- controllers/tagsController.js
+++ controllers/tagsController.js
@@ -1,5 +1,18 @@
 const Project = require('../models/project')
 
+/**
+ * @api {get} /tags List all tags
+ * @apiName GetTags
+ * @apiGroup Tags
+ *
+ * @apiSuccessExample SuccessResponse
+ *   HTTP/1.1 200 OK
+ *   [
+ *     "alpha",
+ *     "beta",
+ *     "gamma"
+ *   ]
+ */
 module.exports.index = (req, res) => {
   Project.find().distinct('tags', (err, tags) => {
     if (err) return res.status(500).send(err)
