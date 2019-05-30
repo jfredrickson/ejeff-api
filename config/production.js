2327dc71585eb92ceda913f97edddf4d4efe6552
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2019-05-29 22:50:39 -0400

    fix stray whitespace

--- config/production.js
+++ config/production.js
@@ -4,13 +4,13 @@ module.exports = new function () {
   this.baseUrl = process.env.BASE_URL
   this.secret = process.env.SECRET
   this.tokenExpiration = process.env.TOKEN_EXPIRATION || '1 day'
-  this. logFormat = process.env.LOG_FORMAT || 'common'
+  this.logFormat = process.env.LOG_FORMAT || 'common'
   this.db = {
     url: process.env.DATABASE_URL
   }
   this.inquiriesMailFrom = process.env.INQUIRIES_MAIL_FROM
   this.inquiriesMailTo = process.env.INQUIRIES_MAIL_TO
-  this. mailTransport = {
+  this.mailTransport = {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
--- config/test.js
+++ config/test.js
@@ -4,13 +4,13 @@ module.exports = new function () {
   this.baseUrl = process.env.BASE_URL || 'http://localhost:' + this.port + '/'
   this.secret = process.env.SECRET || 'abc123'
   this.tokenExpiration = '1 day'
-  this. logFormat = 'dev'
+  this.logFormat = 'dev'
   this.db = {
     url: process.env.DATABASE_URL || 'mongodb://localhost/ejeff-test'
   }
   this.inquiriesMailFrom = 'from@example.net'
   this.inquiriesMailTo = 'to@example.com'
-  this. mailTransport = {
+  this.mailTransport = {
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
